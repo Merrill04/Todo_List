24ca8faac9ceaff4b9910adfee12c3ee25d8d01b
Merrill04 <merrilldmonte@gmail.com>
2026-05-22 11:02:53 +0530

    Initialized the frontend and backend

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="text-center bg-black">
+        <h1 className="font-extrabold text-purple-300"> Todo App</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
